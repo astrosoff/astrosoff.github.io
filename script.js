f7083b02d3eb534607e533a32924ee250244e33a
astrosoff <sofian.bouhlou@gmail.com>
2024-10-15 19:26:13 +0200

    feat: game.

--- script.js
+++ script.js
@@ -1,38 +1,69 @@
-// NUMBER GUESSING GAME
+const hint = document.getElementById("hint");
+const noOfGuessesRef = document.getElementById("no-of-guesses");
+const guessedNumsRef = document.getElementById("guessed-nums");
+const restartButton = document.getElementById("restart");
+const game = document.getElementById("game");
+const guessInput = document.getElementById("guess");
+const checkButton = document.getElementById("check-btn");
 
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+let answer, noOfGuesses, guessedNumsArr;
 
-let attempts = 0;
-let guess;
-let running = true;
+const play = () => {
+  const userGuess = guessInput.value;
+  if (userGuess < 1 || userGuess > 100 || isNaN(userGuess)) {
+    alert("Please enter a valid number between 1 and 100.");
+    return;
+  }
+  guessedNumsArr.push(userGuess);
+  noOfGuesses += 1;
+  if (userGuess != answer) {
+    if (userGuess < answer) {
+      hint.innerHTML = "Too low. Try Again!";
+    } else {
+      hint.innerHTML = "Too high. Try Again!";
+    }
+    noOfGuessesRef.innerHTML = `<span>No. Of Guesses:</span> ${noOfGuesses}`;
+    guessedNumsRef.innerHTML = `<span>Guessed Numbers are: </span>${guessedNumsArr.join(
+      ","
+    )}`;
+    hint.classList.remove("error");
+    setTimeout(() => {
+      hint.classList.add("error");
+    }, 10);
+  } else {
+    hint.innerHTML = `Congratulations!<br>The number was <span>${answer}</span>.<br>You guessed the number in <span>${noOfGuesses} </span>tries.`;
+    hint.classList.add("success");
+    game.style.display = "none";
+    restartButton.style.display = "block";
+  }
+};
 
-while(running){
+const init = () => {
+  console.log("Game Started");
+  answer = Math.floor(Math.random() * 100) + 1;
+  console.log(answer);
+  noOfGuesses = 0;
+  guessedNumsArr = [];
+  noOfGuessesRef.innerHTML = "No. Of Guesses: 0";
+  guessedNumsRef.innerHTML = "Guessed Numbers are: None";
+  guessInput.value = "";
+  hint.classList.remove("success", "error");
+};
 
-	guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
-	guess = Number(guess);
-	
-	if(isNaN(guess)) {
-		window.alert("Please enter a valid number");
-	}
-	else if(guess < minNum || guess > maxNum) {
-		window.alert("Please enter a valid number");
-	}
-	else{
-		attempts++;
-		if(guess < answer) {
-			window.alert("TOO LOW! TRY AGAIN!");
-		}
-		else if(guess > answer) {
-			window.alert("TOO HIGH! TRY AGAIN!");
-		}
-		else {
-			window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts`);
-			running = false;
-		}
-	}
-}
+guessInput.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    play();
+  }
+});
 
+restartButton.addEventListener("click", () => {
+  game.style.display = "grid";
+  restartButton.style.display = "none";
+  hint.innerHTML = "";
+  hint.classList.remove("success");
+  init();
+});
 
-
+checkButton.addEventListener("click", play);
+window.addEventListener("load", init);
